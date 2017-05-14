8995c68938b0544417874e3274e386d25aa7181d
Josh Barrington <josh.s.barrington@gmail.com>
2017-05-14 18:58:54 +0100

    First commit

---
+++ motor-control.py
@@ -0,0 +1,9 @@
+import explorerhat
+
+class Motors:
+
+    def forwards(self):
+        explorerhat.motor.one.forwards()
+        explorerhat.motor.one.forwards()
+    def backwards(self):
+	explorerhat    
